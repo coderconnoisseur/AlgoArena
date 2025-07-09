9dee182861c2a1d142ebdfaeb18cf9fa836da441
coderconnoisseur <142653086+coderconnoisseur@users.noreply.github.com>
2025-07-09 16:18:04 +0530

    requirements.txt

--- app/schema/user.py
+++ app/schema/user.py
@@ -5,12 +5,12 @@ from datetime import datetime
 class User(UserBase):
     username: str
     email: EmailStr
+    password: str
     id: int
     xp: int = 0
     solved_problems: List[int] = []
     created_problems: List[int] = []
     is_active: bool = True
     created_at: datetime
-
     class Config:
         orm_mode = True
