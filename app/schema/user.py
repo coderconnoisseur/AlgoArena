b5208313ee7798a5c83b5ae4b1bc69a4cbd7d890
coderconnoisseur <142653086+coderconnoisseur@users.noreply.github.com>
2025-07-09 16:33:07 +0530

    fixed user schema

--- app/schema/user.py
+++ app/schema/user.py
@@ -2,15 +2,21 @@ from pydantic import BaseModel, EmailStr
 from typing import List
 from datetime import datetime
 
-class User(UserBase):
+class UserBase(BaseModel):
     username: str
     email: EmailStr
+
+class UserCreate(UserBase):
     password: str
+
+class User(UserBase):
+    
     id: int
     xp: int = 0
     solved_problems: List[int] = []
     created_problems: List[int] = []
     is_active: bool = True
     created_at: datetime
+
     class Config:
         orm_mode = True
